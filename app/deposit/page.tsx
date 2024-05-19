2e7c3b7d6ddcfcd23c6fc7225828d9b3bae94858
PrinGamingYT <sunpixelstudios1245@gmail.com>
2024-05-19 11:23:59 +0700

    made the deposit page and added the video to the home page.

--- app/deposit/page.tsx
+++ app/deposit/page.tsx
@@ -3,11 +3,24 @@ import { Title } from "@/components/aboutus"
 export default function () {
     return (
         <div>
-            <Title>Payment form</Title>
-            <div>Full Name: </div>
-            <input type="text" id="fname" name="firstname" placeholder="Your Name"></input><br></br>
-            <label for="cardnumber">Card Number: </label>
-            <input type="text" id="cardnumber" name="cardnumber" placeholder="XXXX XXXX XXXX XXXX"></input><br></br>
+            <h2 className="text-4xl font-bold text-center">Deposit</h2>
+            <div className="container mx-auto px-4 py-8">
+                <div className="flex flex-col">
+                    <label for="amount" class="text-gray-700">Amount:</label>
+                    <input type="number" id="amount" name="amount" required className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                </div>
+                <div className="flex flex-col">
+                    <label for="card-number" className="text-gray-700">Card Number:</label>
+                    <input type="number" id="card-number" name="card-number" maxlength="16" inputmode="numeric" placeholder="XXXX XXXX XXXX XXXX" className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                </div>
+                    <div className='credit-card-info flex justify-between text-sm text-gray-500'>
+                    <span>Expires: </span>
+                    <input type="number" id="expire-date" name="expire-date" placeholder="MM" className="px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input><div>/</div><input type="number" id="expire-date" name="expire-date" placeholder="YYYY" className="px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                    <label for="cvv" class="text-gray-700">CVV:</label>
+                    <input type="number" id="cvv" name="cvv" maxlength="3" inputmode="numeric" placeholder="123" className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                </div>
+                <button type="submit" className="w-full py-2 rounded-md bg-sky-500 text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">Deposit</button>
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 
+
 export default function Home() {
   return (
-    <div>
+    <div className='mt-6 p-6 pl-0 flex flex-row items-center justify-evenly border-b-white rounded-[1100px]  border-0 border-b-2 max-w-[1100px] mx-auto'>
+      <Link href='/'>
+        <Image src='/logo.png' className='hover:scale-110 duration-500' height={200} width={200} alt='logo'></Image>
+      </Link>
       <br></br>
-      <Link href="/policy" className="font-bold p-6">Term and Condition of Use</Link>
-      <input type="checkbox" name = "finished"></input>
-      <label htmlFor = 'finished' className="p-2">accept our terms and agreement?</label>
+      <div className="font-bold">Our Promotional Video</div>
+      <video width="640" height="480" controls>
+        <source src={ "/Freelabubu.mp4" } type="video/mp4"/>
+      </video>
     </div>
   );
 }
