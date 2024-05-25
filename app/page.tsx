dd0bb7bf9024c509b792a2514d936f9b45f47f31
PrinGamingYT <sunpixelstudios1245@gmail.com>
2024-05-25 17:51:13 +0700

    Pitch like femboys

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Home() {
   return (
-    <div className='mt-6 p-6 pl-0 flex flex-row items-center justify-evenly border-b-white rounded-[1100px]  border-0 border-b-2 max-w-[1100px] mx-auto'>
-      <Link href='/'>
-        <Image src='/logo.png' className='hover:scale-110 duration-500' height={200} width={200} alt='logo'></Image>
+    <div
+      className="mt-6 p-6 pl-0 flex flex-row items-center justify-evenly border-b-white rounded-[1100px] border-0 border-b-2 max-w-[1100px] mx-auto"
+    >
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          className="hover:scale-110 duration-500"
+          height={200}
+          width={200}
+          alt="logo"
+        ></Image>
       </Link>
-      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
-      <br></br>
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
       <div className="font-bold">Our Promotional Video</div>
       <video width="640" height="480" controls>
-        <source src={ "/Freelabubu.mp4" } type="video/mp4"/>
+        <source src={"/Freelabubu.mp4"} type="video/mp4" />
       </video>
     </div>
   );
