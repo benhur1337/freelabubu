1136582810a77b8629e898fd334a7a31bdc99c8c
PrinGamingYT <sunpixelstudios1245@gmail.com>
2024-08-14 21:42:58 +0700

    fixed the deposit page not working on text with white background

--- app/deposit/page.tsx
+++ app/deposit/page.tsx
@@ -1,4 +1,4 @@
-import { Title } from "@/components/aboutus"
+import React from 'react';
 
 export default function Page() {
     return (
@@ -6,20 +6,20 @@ export default function Page() {
             <h2 className="text-4xl font-bold text-center p-6">Deposit</h2>
             <div className="container mx-auto px-4 py-8 p-6">
                 <div className="flex flex-col p-6">
-                    <label for="amount" class="text-gray-700">Amount:</label>
-                    <input type="number" id="amount" name="amount" required className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                    <label htmlFor="amount" className="text-gray-700">Amount:</label>
+                    <input type="number" id="amount" name="amount" required className="text-black px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
                 </div>
                 <div className="flex flex-col p-6">
-                    <label for="card-number" className="text-gray-700">Card Number:</label>
-                    <input type="number" id="card-number" name="card-number" maxlength="16" inputmode="numeric" placeholder="XXXX XXXX XXXX XXXX" className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                    <label htmlFor="card-number" className="text-gray-700">Card Number:</label>
+                    <input type="number" id="card-number" name="card-number" min="1" max="16" placeholder="XXXX XXXX XXXX XXXX" className="text-black px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
                 </div>
                     <div className='credit-card-info flex justify-between text-sm text-gray-500 p-6'>
                     <span>Expires: </span>
-                    <input type="number" id="expire-date" name="expire-date" placeholder="MM" className="px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input><div>/</div><input type="number" id="expire-date" name="expire-date" placeholder="YYYY" className="px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
-                    <label for="cvv" class="text-gray-700">CVV:</label>
-                    <input type="number" id="cvv" name="cvv" maxlength="3" inputmode="numeric" place3holder="123" className="p-6 px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                    <input type="number" id="expire-date" name="expire-date" placeholder="MM" className="text-black px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input><div>/</div><input type="number" id="expire-date" name="expire-date" placeholder="YYYY" className="px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
+                    <label htmlFor="cvv" className="text-gray-700">CVV:</label>
+                    <input type="number" id="cvv" name="cvv" max="3" placeholder="123" className="text-blackp-6 px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-sky-500"></input>
                 </div>
-                <button type="submit" className="p-6 w-full py-2 rounded-md bg-sky-500 text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">Deposit</button>
+                <button type="submit" className="text-blackp-6 w-full py-2 rounded-md bg-sky-500 text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">Deposit</button>
             </div>
         </div>
     )
